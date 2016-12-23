d6ee4152cca0088e6b9871388ba7bd5aa4dc678c
ricardogcolombo <ricardogcolombo@gmail.com>
2016-12-23 12:18:25 -0300

    move miniature server to other repo

--- server/app/models/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  "directory": "./bookDir",
-  "database": "",
-  "port": process.env.PORT || 3000
-};
--- server/config/enviroments/development.js
+++
@@ -1,8 +0,0 @@
-var nconf = require('nconf');
-//TODO Configure this
-nconf.set('url', 'mywebsite.com');
-// nconf.set('database', {
-  // user: 'username',
-  // password: 'password',
-  // server: 'url'
-// });
